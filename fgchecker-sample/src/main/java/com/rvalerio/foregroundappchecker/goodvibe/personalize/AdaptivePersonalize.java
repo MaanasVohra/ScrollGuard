70f318b2a6818a05dde0d2280ed19ac9043406b2
Fabian Okeke <fnokeke@gmail.com>
2017-06-20 01:38:25 -0400

    Adaptive Personalized should use the last 7 days of data but if user doesn't have up to 7 days then use the data available; also start computing daily adaptive average even before treatment period starts

--- fgchecker-sample/src/main/java/com/rvalerio/foregroundappchecker/goodvibe/personalize/AdaptivePersonalize.java
+++ fgchecker-sample/src/main/java/com/rvalerio/foregroundappchecker/goodvibe/personalize/AdaptivePersonalize.java
@@ -15,7 +15,7 @@ import java.util.Date;
  */
 
 public class AdaptivePersonalize extends StaticPersonalize {
-    private static final int LAST_N_DAYS = 1; //// FIXME: 6/18/17 change back to 7
+    private static final int LAST_N_DAYS = 7;
 
     public AdaptivePersonalize(Context context, String treatmentStartDateStr) {
         super(context, treatmentStartDateStr);
@@ -23,29 +23,34 @@ public class AdaptivePersonalize extends StaticPersonalize {
 
     public void addDataPoint(int timeSpent, int noOfOpen) {
         insertDataIntoStore(timeSpent, noOfOpen);
-        if (canComputeNewAvg(getTreatStart())) {
-            computeAndStoreNewAverage(ALL_TIME_SPENT, CURRENT_AVG_TIME_SPENT, LAST_N_DAYS);
-            computeAndStoreNewAverage(ALL_NUM_OF_OPENS, CURRENT_AVG_NUM_OF_OPENS, LAST_N_DAYS);
-        }
+        computeAndStoreNewAverage(ALL_TIME_SPENT, CURRENT_AVG_TIME_SPENT, LAST_N_DAYS);
+        computeAndStoreNewAverage(ALL_NUM_OF_OPENS, CURRENT_AVG_NUM_OF_OPENS, LAST_N_DAYS);
+//        if (canComputeNewAvg(getTreatStart())) {
+//            computeAndStoreNewAverage(ALL_TIME_SPENT, CURRENT_AVG_TIME_SPENT, LAST_N_DAYS);
+//            computeAndStoreNewAverage(ALL_NUM_OF_OPENS, CURRENT_AVG_NUM_OF_OPENS, LAST_N_DAYS);
+//        }
     }
 
     private void computeAndStoreNewAverage(String storeKey, String avgKey, int lastKDays) {
-        JSONArray allTimeSpent = Store.getJsonArray(getCxt(), storeKey);
-        if (allTimeSpent.length() == 0) return;
+        JSONArray allStoredValues = Store.getJsonArray(getCxt(), storeKey);
+        if (allStoredValues.length() == 0) return;
         float total = 0;
-        int limit = lastKDays <= allTimeSpent.length() ? lastKDays : allTimeSpent.length();
-        for (int i = limit - 1; i >= 0; i--) {
-            total += allTimeSpent.optInt(i);
+        int limit = lastKDays <= allStoredValues.length() ? lastKDays : allStoredValues.length();
+        int lastIndex = allStoredValues.length() - 1;
+        int value;
+        for (int i = 0; i < limit; i++) {
+            value = allStoredValues.optInt(lastIndex - i);
+            total += value;
         }
         Store.setInt(getCxt(), avgKey, Math.round(total / limit));
     }
 
-    private static boolean canComputeNewAvg(String treatmentStartDateStr) {
-        Date treatmentStartDate = DateHelper.strToDate(treatmentStartDateStr);
-        Date today = DateHelper.strToDate(DateHelper.getTodayDateStr());
-        final long oneDayInMillis = 24 * 60 * 60 * 1000;
-        long diffInDays = (today.getTime() - treatmentStartDate.getTime()) / oneDayInMillis;
-        return diffInDays > LAST_N_DAYS;
-    }
+//    private static boolean canComputeNewAvg(String treatmentStartDateStr) {
+//        Date treatmentStartDate = DateHelper.strToDate(treatmentStartDateStr);
+//        Date today = DateHelper.strToDate(DateHelper.getTodayDateStr());
+//        final long oneDayInMillis = 24 * 60 * 60 * 1000;
+//        long diffInDays = (today.getTime() - treatmentStartDate.getTime()) / oneDayInMillis;
+//        return diffInDays > LAST_N_DAYS;
+//    }
 }
 
